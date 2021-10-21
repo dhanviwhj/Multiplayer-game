28f039a3dcf8d24b7ea38cc4a8d0372eb850cba0
dhanviwhj <81857308+dhanviwhj@users.noreply.github.com>
2021-10-21 13:59:49 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Multiplayer-game
+# C36RV_SpeedRacer_StudentActivity
+Student Activity
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas,bgImg;
+var gameState = 0;
+var playerCount,database;
+var form,player,game;
+
+function setup(){
+ canvas = createCanvas(400,400)
+
+ database = firebase.database();
+
+}
+
+function draw(){
+ 
+
+
+}
+
